a17d0bf2229916e5805e6f10318613d1fdbbcaf1
jayrpl <jadenpalmerleandre@hotmail.co.uk>
2024-12-06 22:58:41 +0000

    Project start

---
+++ coding-tracker/coding-tracker/CodingSession.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace coding_tracker
+{
+    record CodingSession(DateTime startTime, DateTime endTime)
+    {
+        private int id { get; set; }
+        private float duration { get; set; }
+
+        internal TimeSpan CalculateDuration()
+        {
+            return (startTime - endTime);
+        }
+    }
---
+++ coding-tracker/coding-tracker/UserInput.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace coding_tracker
+{
+    // Class responsible for receiving user input and validating it
+    class UserInput
+    {
+        internal static string? GetStringInput()
+    }
+}
